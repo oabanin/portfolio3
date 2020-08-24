1da8addbb3626b6aa36cd07ee97324e3e012a268
oabanin-HOME <juristaba@gmail.com>
2020-08-24 23:11:24 +0300

    add mini-css-extract-plugin

--- src/index.js
+++ src/index.js
@@ -10,28 +10,15 @@ import $ from 'jquery';
 import 'jquery-mousewheel';
 import 'jquery-touchswipe'; 
 import 'slick-carousel';
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 
 
-  // function component() {
-  //   const element = document.createElement('div');
-  //   const btn = document.createElement("button");
+const initPageSlider = () => {
+    $('.page-slider--js').slick();
 
-  //   btn.innerHTML="Click me";
-  //   btn.onclick = printF;
-    
 
-  //   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  //   element.classList.add('hello')
+};
 
-  //   const myImage = new Image();
-  //   myImage.src=Icon;
-  //   element.appendChild(myImage);
-  //   element.appendChild(btn);
-
-  //   return element;
-  // }
-  
-  // document.body.appendChild(component());
+$(document).ready(function (){
+  initPageSlider();
+});
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 // We need Nodes fs module to read directory contents
 const fs = require('fs');
 
 // Our function that generates our html plugins
-function generateHtmlPlugins (templateDir) {
+function generateHtmlPlugins(templateDir) {
   // Read files in template directory
   const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
   return templateFiles.map(item => {
@@ -27,13 +28,21 @@ function generateHtmlPlugins (templateDir) {
 const htmlPlugins = generateHtmlPlugins('./src/pages')
 
 module.exports = {
-  entry: {  
-      application: './src/index.js',
-      print: './src/print.js',
-    },  
+  entry: {
+    application: './src/index.js',
+    print: './src/print.js',
+    //style: './src/sass/style.scss'
+  },
   mode: 'development',
   plugins: [
-  	new CleanWebpackPlugin(),
+    new CleanWebpackPlugin(),
+    
+    new MiniCssExtractPlugin({
+      filename:  '[name].[hash].css',
+      chunkFilename:  '[id].[hash].css',
+    }),
+
+    
     // new HtmlWebpackPlugin({
     //   template: './src/_layouts/home.pug',
     // }),
@@ -51,64 +60,85 @@ module.exports = {
 
   module: {
     rules: [
-       {
-         test: /\.css$/,
-         use: [
-           'style-loader',
-           'css-loader',
-         ],
-       },
-       {
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
         test: /\.(png|jpe?g|gif)$/i,
         loader: 'file-loader',
-  	      options: {
-  	        name: '[name].[ext]',
-  	      },
+        options: {
+          name: '[name].[ext]',
         },
-      	{ 
+      },
+      {
         test: /\.pug$/,
         use: [
           {
-          loader: 'pug-loader',
+            loader: 'pug-loader',
             options: {
               pretty: true
             }
           },
         ]
-        },
-        {
+      },
+      {
         test: /\.svg$/,
         loader: 'svg-inline-loader'
-        },
-      	{
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader',
-          ],
-        },
-        {
-        test: /\.s[ac]ss$/i,
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
-          // Creates `style` nodes from JS strings
-          'style-loader',
-          // Translates CSS into CommonJS
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
-          // Compiles Sass to CSS
-          {
-            loader: 'sass-loader',
-            options: {
-              sourceMap: true,
-            },
-          },
+          'file-loader',
         ],
-        },
-     ],
-   },
+      },
+
+      // //Compile SCSS to separateCSS without Plugin
+      // {  
+      //   test: /\.scss$/,
+      //   exclude: /node_modules/,
+      //   use: [
+      //     {
+      //       loader: 'file-loader',
+      //       options: { outputPath: '', name: '[name].min.css' }
+      //     },
+      //     'sass-loader'
+      //   ]
+      // },
+
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+
+      // //SCSS to js bundle
+      // {
+      //   test: /\.s[ac]ss$/i,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     'style-loader',
+      //     // Translates CSS into CommonJS
+      //     {
+      //       loader: 'css-loader',
+      //       options: {
+      //         sourceMap: true,
+      //       },
+      //     },
+      //     {
+      //       loader: 'sass-loader', // Compiles Sass to CSS
+      //       options: {
+      //         sourceMap: true,
+      //       },
+      //     },
+      //   ],
+      // },// End SCSS to js bundle
+
+    ],//End rules
+  },//End Module
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
