b921d75ab184d1e2887b726c75b810037c2846b6
oabanin-WORK <juristaba@gmail.com>
2020-08-24 16:58:27 +0400

    add to webpack.config.js multiple files for PUG

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+// We need Nodes fs module to read directory contents
+const fs = require('fs');
+
+// Our function that generates our html plugins
+function generateHtmlPlugins (templateDir) {
+  // Read files in template directory
+  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir))
+  return templateFiles.map(item => {
+    // Split names and extension
+    const parts = item.split('.');
+    const name = parts[0];
+    const extension = parts[1];
+    // Create new HTMLWebpackPlugin with options
+    return new HtmlWebpackPlugin({
+      filename: `${name}.html`,
+      template: path.resolve(__dirname, `${templateDir}/${name}.${extension}`)
+    })
+  })
+}
+
+// Call our function on our views directory.
+const htmlPlugins = generateHtmlPlugins('./src/_layouts')
+
 module.exports = {
   entry: {  
       application: './src/index.js',
@@ -10,14 +33,16 @@ module.exports = {
   mode: 'development',
   plugins: [
   	new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.pug',
-    }),
-    new HtmlWebpackPlugin({
-    filename: 'about.html',
-    template: './src/about.pug'
-  })
-  ],
+    // new HtmlWebpackPlugin({
+    //   template: './src/_layouts/home.pug',
+    // }),
+    // new HtmlWebpackPlugin({
+    // filename: 'contact.html',
+    // inject: false,
+    // template: './src/_layouts/contact.pug'
+    // })
+
+  ].concat(htmlPlugins),
   output: {
     path: path.resolve(__dirname, 'dist'), //ПАПКА куда кладет файл js
     filename: '[name].bundle.js' //имя СБОРКи
@@ -82,6 +107,7 @@ module.exports = {
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
     compress: true,
+    overlay: true,
     port: 9000,
     open: true //открыть браузер по окончанию старта сервера
   }
