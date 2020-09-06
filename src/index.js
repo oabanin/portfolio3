4103bd69915f37e64c013e73107651c86f9de7a8
oabanin-HOME <juristaba@gmail.com>
2020-09-06 21:54:35 +0300

    add action to nav menu. add draft for changing menu on scroll

--- src/index.js
+++ src/index.js
@@ -11,6 +11,21 @@ import arrowLeft from "./img/arrow-left.svg";
 const initPageSlider = () => {
 
   const $slider = $('.page-slider--js');
+  const $navToggle = $('.nav-toggle');
+  const $perspective__menu = $('.perspective__menu');
+  const $perspective = $('.perspective');
+  const $header__hirebutton = $('.header__hire-button');
+
+  
+
+  
+  
+
+
+
+  // children().each(function(num, node){
+  //   console.log(node);
+  // })
 
   $slider.slick({
     slidesToShow: 1,
@@ -20,7 +35,7 @@ const initPageSlider = () => {
     touchTreshold: 800,
     vertical: true,
     infinite: false,
-    verticalSwiping:true,
+    verticalSwiping: true,
   });
 
   $slider.mousewheel(function (e) {
@@ -28,6 +43,8 @@ const initPageSlider = () => {
     if (e.deltaY < 0) {
       $(this).slick('slickNext');
       $(this).slick('setPosition');
+      
+
     }
     else {
       $(this).slick('slickPrev');
@@ -35,26 +52,32 @@ const initPageSlider = () => {
     }
   });
 
+  //Click toggle
+  $navToggle.click(function () {
+    $perspective__menu.addClass('is_visible');
+    $perspective.addClass('is-active');
+  })
+
 
 };
 
 const initWorkSlider = () => {
   const $slider = $('.js-work-slider'),
-  		$slide1 = $slider.find('.js-slide-1').html(),
-  		$slide2 = $slider.find('.js-slide-2').html(),
-  		$slide3 = $slider.find('.js-slide-3').html();
+    $slide1 = $slider.find('.js-slide-1').html(),
+    $slide2 = $slider.find('.js-slide-2').html(),
+    $slide3 = $slider.find('.js-slide-3').html();
 
- $slider.html(' ');
- $('<div>').appendTo($slider).append($slide1).append($slide2).append($slide3);
- $('<div>').appendTo($slider).append($slide2).append($slide3).append($slide1);
- $('<div>').appendTo($slider).append($slide3).append($slide1).append($slide2);
+  $slider.html(' ');
+  $('<div>').appendTo($slider).append($slide1).append($slide2).append($slide3);
+  $('<div>').appendTo($slider).append($slide2).append($slide3).append($slide1);
+  $('<div>').appendTo($slider).append($slide3).append($slide1).append($slide2);
 
   $slider.slick({
-  	"fade":true,
-  	"prevArrow": '<span class="work-arrow is-prev"><img src="' + arrowLeft +'"></span>',
-  	"nextArrow": '<span class="work-arrow is-next"><img src="' + require("./img/arrow-right.svg").default + '"></span>'
+    "fade": true,
+    "prevArrow": '<span class="work-arrow is-prev"><img src="' + arrowLeft + '"></span>',
+    "nextArrow": '<span class="work-arrow is-next"><img src="' + require("./img/arrow-right.svg").default + '"></span>'
   }
-  	);
+  );
 
 }
 
@@ -65,5 +88,28 @@ $(document).ready(function () {
   // $('input').click(function(event){
   //   event.target.focus();
   // })
+
+
+  $(window).scroll(function(a){
+    // Get container scroll position
+    var fromTop = $(this).scrollTop();
+    console.log(a);
+    // Get id of current scroll item
+    // var cur = scrollItems.map(function(){
+    //   if ($(this).offset().top < fromTop)
+    //     return this;
+    // });
+    // // Get the id of the current element
+    // cur = cur[cur.length-1];
+    // var id = cur && cur.length ? cur[0].id : "";
+    
+    // if (lastId !== id) {
+    //     lastId = id;
+    //     // Set/remove active class
+    //     menuItems
+    //       .parent().removeClass("active")
+    //       .end().filter("[href='#"+id+"']").parent().addClass("active");
+    // }                   
+ });
 });
 
